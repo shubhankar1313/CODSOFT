8cd2f6cbc26e36c234dc206d5f4910c7d23759b0
shubhankar1313 <130796984+shubhankar1313@users.noreply.github.com>
2023-09-05 16:45:14 +0530

    Alt_1

--- todo.py
+++ todo.py
@@ -2,68 +2,44 @@ import string
 from tkinter import *
 import tkinter.font as font
 
-task_pos = 5
+todo, data = [], ""
 
-def remove_task(edit_pos):
-    text1.destroy
-    button_edit1.destroy()
-    button_remove1.destroy()
-    
-def submit_edit(edit_pos):
-    global button1
-
-    text1.destroy()
-
-    text_edit = Label(root, fg = "white", bg = "black", text = line.get(), font = font_task)
-    text_edit.grid(row = edit_pos, column = 0, sticky = 'w')
-
-    button2.destroy()
-
-    font_button = font.Font(family = "Bell Gothic Std Black", size = 15)
-    button1 = Button(root, text = "Submit", fg = "black", bg = "white", command = add_task, font = font_button)
-    button1.grid(row = 3, column = 1, padx = 20)
-    
-def edit_task(entry, edit_pos):
-    global button2
-
-    line.set(entry)
-
-    button1.destroy()
-
-    font_button = font.Font(family = "Bell Gothic Std Black", size = 15)
-    button2 = Button(root, text = "Edit", fg = "black", bg = "white", command = lambda: submit_edit(edit_pos), font = font_button)
-    button2.grid(row = 3, column = 1, padx = 20)
+def remove_all():
+    global data
+    data = ""
+    expression2.insert('end -1 chars', data)
     
 def add_task():
-    global task_pos, text1, font_task, button_edit, button_remove
+    global todo, data, text_error
 
-    font_task = font.Font(family = "Bell Gothic Std Black", size = 15)
-    locals()[f"text{task_pos-4}"] = Label(root, fg = "white", bg = "black", text = str(line.get()), font = font_task)
-    locals()[f"text{task_pos-4}"].grid(row = task_pos, column = 0, sticky = 'w')
+    text_error.destroy()
 
-    edit_pos = task_pos-4
-    font_edit = font.Font(family = "Bell Gothic Std Black", size = 15)
-    locals()[f"button_edit{task_pos-4}"] = Button(root, text = "Edit", fg = "black", bg = "green", command = lambda: edit_task(edit_pos), font = font_button)
-    locals()[f"button_edit{task_pos-4}"].grid(row = task_pos, column = 1, padx = 20)
+    if line.get() != "":
+        todo.append(line.get())
 
-    font_remove = font.Font(family = "Bell Gothic Std Black", size = 15)
-    locals()[f"button_remove{task_pos-4}"] = Button(root, text = "Remove", fg = "black", bg = "red", command = lambda: remove_task(edit_pos), font = font_button)
-    locals()[f"button_remove{task_pos-4}"].grid(row = task_pos, column = 2, padx = 20)
-    
-    line.set("")
-    
-    task_pos += 1
-    
+        for task in todo:
+            data += f"{task}\n"
+
+        expression2.insert('end -1 chars', data)
+
+        line.set("")
+        todo, data = [], ""
+
+    else:
+        text_error = Label(root, fg = "red", bg = "black", text = "Enter task!", font = font_entry)
+        text_error.grid(row = 4, column = 0, sticky = 'w', padx = 200)
+
+
 if __name__ == "__main__":
     root = Tk()
     root.configure(background = "black")
     root.title("To-Do List")
-    root.geometry("695x358")
+    root.geometry("595x730")
 
     font_title = font.Font(family = "Bell Gothic Std Black", size = 25, weight = "bold")
     font_title.configure(underline = True)
     title = Label(root, fg = "white", bg = "black", text = "To-Do List\n", font = font_title)
-    title.grid(row = 0, column = 0)
+    title.grid(row = 0, column = 0, sticky = 'w', padx = 200)
     
     font_subtitle = font.Font(family = "Bell Gothic Std Black", size = 20)
     text_add = Label(root, fg = "white", bg = "black", text = "Add Items", font = font_subtitle)
@@ -74,15 +50,39 @@ if __name__ == "__main__":
     line = StringVar(root)
 
     font_entry = font.Font(family = "Bell Gothic Std Black", size = 15)
-    expression = Entry(root, textvariable = line, font = font_entry)
-    expression.grid(row = 3, column = 0, ipadx = 120, ipady = 5)
+    expression1 = Entry(root, textvariable = line, font = font_entry)
+    expression1.grid(row = 3, column = 0, ipadx = 120, ipady = 5, padx = 10, sticky = 'w')
 
     font_button = font.Font(family = "Bell Gothic Std Black", size = 15)
     button1 = Button(root, text = "Submit", fg = "black", bg = "white", command = add_task, font = font_button)
-    button1.grid(row = 3, column = 1, padx = 20)
+    button1.grid(row = 3, column = 0, padx = 500)
 
     font_subtitle = font.Font(family = "Bell Gothic Std Black", size = 20)
     text_task = Label(root, fg = "white", bg = "black", text = "\nTasks", font = font_subtitle)
     text_task.grid(row = 4, column = 0, sticky = 'w')
 
+    todolist = StringVar(root)
+
+    font_entry = font.Font(family = "Bell Gothic Std Black", size = 15)
+    expression2 = Text(root, height = 5, width = 20, font = font_entry)
+    expression2.grid(row = 5, column = 0, ipadx = 120, ipady = 150, padx = 10, sticky = 'w')
+
+    text_error = Label(root, fg = "red", bg = "black", text = "   ", font = font_entry)
+    text_error.grid(row = 4, column = 0, sticky = 'w', padx = 200)
+
+    text_empty = Label(root, fg = "red", bg = "black", text = "   ", font = font_entry)
+    text_empty.grid(row = 6, column = 0, sticky = 'w', padx = 200)
+
+    button2 = Button(root, text = "Remove All", fg = "black", bg = "red", command = remove_all, font = font_button)
+    button2.grid(row = 7, column = 0, sticky = 'w', padx = 20)
+
+    button3 = Button(root, text = "Remove Entry:", fg = "black", bg = "red", command = remove_all, font = font_button)
+    button3.grid(row = 7, column = 0, sticky = 'w', padx = 200)
+
+    remove_entry = IntVar(root)
+    expression3 = Text(root, height = 1, width = 3, font = font_entry)
+    expression3.grid(row = 7, column = 0, ipadx = 1, ipady = 1, padx = 360)
+
     root.mainloop()
+
+    
